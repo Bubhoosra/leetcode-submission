95bd2e6ef1b22e408870abec45b6106d138c6995
Bubhoosra <120300546+Bubhoosra@users.noreply.github.com>
2023-09-28 12:08:39 +0530

    Time: 102 ms (88.53%), Space: 19.5 MB (13.78%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.py
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -3,19 +3,47 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+def findmid(head):
+    slow=head
+    fast=head
+    while(fast.next!= None and fast.next.next != None):
+        slow=slow.next
+        fast=fast.next.next
+    return slow
+def merge(head):
+    if head.next ==None:
+        return head
+    mid=findmid(head)
+    afterMid=mid.next
+    mid.next=None
+    head1=merge(head)
+    head2=merge(afterMid)
+    finalHead=mergeSort(head1,head2)
+    return finalHead
+def mergeSort(head1,head2):
+    final=ListNode(0)
+    temp=final
+    while(head1!=None and head2!=None):
+        if(head1.val<=head2.val):
+            temp.next=ListNode(head1.val)
+            head1=head1.next
+        else:
+            temp.next=ListNode(head2.val)
+            head2=head2.next
+        temp=temp.next
+    while(head1!=None):
+        temp.next=ListNode(head1.val)
+        temp=temp.next
+        head1=head1.next
+    while(head2!=None):
+        temp.next=ListNode(head2.val)
+        temp=temp.next
+        head2=head2.next
+    return final.next
+        
+
+
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        l=[]
-        temp=head
-        while(temp!=None):
-            l.append(temp.val)
-            temp=temp.next
-        l=sorted(l)
-        temp=head1=ListNode(0)
-        i=0
-        n=len(l)
-        while(i<n):
-            temp.next=ListNode(l[i])
-            temp=temp.next
-            i+=1
-        return head1.next
+        return merge(head)
+        
