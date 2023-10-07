71dc81f8adaa19471dc8cb5d6725630cfb1a7c63
Bubhoosra <120300546+Bubhoosra@users.noreply.github.com>
2023-10-07 14:24:00 +0530

    Time: 48 ms (17.41%), Space: 16.6 MB (9.08%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,21 +1,51 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        res=0
-        j=0
-        for i in range(len(a)-1,-1,-1):
-
-
-            p=int(a[i])*(2**j)
+        i=len(a)
+        j=len(b)
+        if i<j:
+            a=str(0)*(j-i)+a
+        elif i>j:
+            b=str(0)*(i-j)+b
+        print(a,b)
+        s=""
+        c=0
+        i=len(a)-1
+        while(i>-1):
+            if a[i]=="1" and b[i]=="1":
+                if c==1:
+                    s="1"+s
+                    c=1
+                else:
+                    s="0"+s
+                    c=1
+            elif a[i]=="1" and b[i]=="0":
+                if c==1:
+                    s="0"+s
+                    c=1
+                else:
+                    s="1"+s
+            elif a[i]=="0" and b[i]=="1":
+                if c==1:
+                    s="0"+s
+                    c=1
+                else:
+                    s="1"+s
+            elif a[i]=="0" and b[i]=="0":
+                if c==1:
+                    s="1"+s
+                    c=0
+                else:
+                    s="0"+s
+                
+            print(s)
+            i-=1
+        if c==1:
+            s="1"+s
+        return s
             
-            res+=p
-            j+=1
-        j=0
-       
-        for i in range(len(b)-1,-1,-1):
-            res+=int(b[i])*(2**j)
-            j+=1
-        
-        ans=str(bin(res))
-        ans=ans.replace("0b","")
-        return ans
+
+
+        print(a,b)
+
+
         
