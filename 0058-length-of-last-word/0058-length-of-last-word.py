d3401ddcdef0b2c888171242568d6941cd48c2e6
Bubhoosra <120300546+Bubhoosra@users.noreply.github.com>
2023-10-10 21:46:22 +0530

    Time: 34 ms (78.97%), Space: 16.2 MB (70.67%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -3,10 +3,10 @@ class Solution:
         res=""
         p=""
         s=s.lower()
-        for i in s:
-            if i==" ":
-                s=s.replace(i,"-1")
-                break
+        # for i in s:
+        #     if i==" ":
+        #         s=s.replace(i,"-1")
+        #         break
         c=0
         for i in s[::-1]:
             if "a"<= i and i<="z":
